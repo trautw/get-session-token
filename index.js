5ffbe3b55ee3c65d1033f9eb759bc767a93d8afb
Christoph Trautwein <trautwein@scientist.com>
2019-10-29 14:22:43 +0100

    ii

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 #!/usr/bin/env node
+"use strict";
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k))
+result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
+
+const otplib = __importStar(require("otplib"));
+
 secret = 'KVKFKRCPNZQUYMLXOVYDSQKJKZDTSRLD'
 otplib.authenticator.generate(secret)
 
