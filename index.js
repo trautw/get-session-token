1b7e18f50a6cd1036d2ad65f4e985d72d3c4a45e
Christoph Trautwein <trautwein@scientist.com>
2019-10-30 10:53:57 +0100

    i

--- index.js
+++ index.js
@@ -1,27 +1,18 @@
 #!/usr/bin/env node
-"use strict";
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k))
-result[k] = mod[k];
-    result["default"] = mod;
-    return result;
-};
-
+const child_process = require('child_process');
 const loadIniFile = require('read-ini-file');
-const otplib = __importStar(require("otplib"));
+const otplib = require("otplib");
 const path = require('path');
 
 const profile = process.argv[2];
-console.log(`Profile: ${profile}`);
 
 const awsSecretsFile = path.join(process.env['HOME'], '.aws', 'credentials');
 const awsSecrets = loadIniFile.sync(awsSecretsFile);
 const profileSecrets = awsSecrets[profile];
-console.log(profileSecrets);
-
 const secret = profileSecrets.aws_mfa_secret;
 const token = otplib.authenticator.generate(secret);
-
 console.log(token);
+
+const sessionInfo = child_process.execSync(`aws sts get-session-token --profile ${profile} --serial-number ${mfaSerial} --token-code ${token}`);
+
+console.log(sessionInfo);
