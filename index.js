519e9e8bd08c44fdc760ff6991217a88e3e92606
Christoph Trautwein <christoph.trautwein@sinnerschrader.com>
2019-11-26 18:15:34 +0100

    use pure ja

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+const AWS = require('aws-sdk');
 const child_process = require('child_process');
 const loadIniFile = require('read-ini-file');
 const otplib = require("otplib");
@@ -22,32 +23,37 @@ const sourceProfileName = awsProfile.source_profile;
 const profileSecrets = awsSecrets[sourceProfileName];
 const mfaSerial = awsConfig[`profile ${sourceProfileName}`].mfa_serial;
 const secret = profileSecrets.aws_mfa_secret;
+
+AWS.config.credentials = new AWS.Credentials(profileSecrets.aws_access_key_id, profileSecrets.aws_secret_access_key);
+
 const token = otplib.authenticator.generate(secret);
+const sts = new AWS.STS();
+var params = {
+  SerialNumber: mfaSerial, 
+  TokenCode: token
+};
+sts.getSessionToken(params, function(err, data) {
+  if (err) console.log(err, err.stack); // an error occurred
+  else {
+    const credentials = data.Credentials;
 
-const sessionInfoString = child_process.execSync(`aws sts get-session-token --profile ${sourceProfileName} --serial-number ${mfaSerial} --token-code ${token}`).toString();
-const sessionInfo = JSON.parse(sessionInfoString);
-const credentials = sessionInfo.Credentials;
+    AWS.config.credentials = new AWS.Credentials(credentials.AccessKeyId, credentials.SecretAccessKey, credentials.SessionToken);
 
-// console.log(sessionInfo);
-// console.log(`export AWS_ACCESS_KEY_ID=${credentials.AccessKeyId}`);
-// console.log(`export AWS_SECRET_ACCESS_KEY=${credentials.SecretAccessKey}`);
-// console.log(`export AWS_SESSION_TOKEN=${credentials.SessionToken}`);
+    var params = {
+      RoleArn: `arn:aws:iam::${awsAccount}:role/${awsRole}`, 
+      RoleSessionName: `${process.env['USER']}-session-${process.env['$']}`
+    };
+    sts.assumeRole(params, function(err, data) {
+      if (err) console.log(err, err.stack); // an error occurred
+      else {
+        const roleCredentials = data.Credentials;
 
-process.env['AWS_ACCESS_KEY_ID'] = credentials.AccessKeyId;
-process.env['AWS_SECRET_ACCESS_KEY'] = credentials.SecretAccessKey;
-process.env['AWS_SESSION_TOKEN'] = credentials.SessionToken;
+        process.env['AWS_ACCESS_KEY_ID'] =     roleCredentials.AccessKeyId;
+        process.env['AWS_SECRET_ACCESS_KEY'] = roleCredentials.SecretAccessKey;
+        process.env['AWS_SESSION_TOKEN'] =     roleCredentials.SessionToken;
 
-// Now assume role
-const roleString = child_process.execSync(`aws sts assume-role --role-arn arn:aws:iam::${awsAccount}:role/${awsRole} --role-session-name $USER-session-$$`).toString();
-const role = JSON.parse(roleString);
-const roleCredentials = role.Credentials;
-
-// console.log(`export AWS_ACCESS_KEY_ID=${roleCredentials.AccessKeyId}`);
-// console.log(`export AWS_SECRET_ACCESS_KEY=${roleCredentials.SecretAccessKey}`);
-// console.log(`export AWS_SESSION_TOKEN=${roleCredentials.SessionToken}`);
-
-process.env['AWS_ACCESS_KEY_ID'] =     roleCredentials.AccessKeyId;
-process.env['AWS_SECRET_ACCESS_KEY'] = roleCredentials.SecretAccessKey;
-process.env['AWS_SESSION_TOKEN'] =     roleCredentials.SessionToken;
-
-child_process.execSync(cmd,{stdio: 'inherit'});
+        child_process.execSync(cmd,{stdio: 'inherit'});
+      }
+    });
+  }
+});
